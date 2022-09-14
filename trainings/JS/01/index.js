a3cd5111405594967033986f3f968ff96628065c
Andres Soto <andressoto@MacBook-Air-de-Edgar.local>
2022-09-14 13:05:11 -0500

    ejercicio js resuelto

---
+++ trainings/JS/01/index.js
@@ -0,0 +1,101 @@
+//Describe what is JavaScript.
+//JavaScript es un lenguaje de programación echo para sitios web.
+
+//Describe what is ES (EcmaScript).
+//ES es una forma de darle mejor funcionalidad a nuestro codigo y también trajo actualizaciones
+//en la cual se agregaron clases, promesas, array function, operadores.
+
+//what and why is the ES version most accepted ?
+//Answer -> ES6 y es la más aceptada
+
+//¿qué es el hoisting?
+//Respuesta -> el hoisting es cuando llamas antes e su declaracion a un funcion o a una variable y despues ser reasignada
+
+//Respuesta -> ¿cuáles son los valores primitivos?
+//Respuesta ->  Number, Undefined, Null, Boolean, String.
+
+//¿cuáles son las diferencias entre let, const y var?
+//Respuesta ->  LET: es un variale en la cual puede cambiar su valor
+//Respuesta -> CONST: es una variable la cual su valor no puede cambiar.
+//Respuesta ->  VAR: es una variable que puede cmbiar su valor, la diferencia entre let y var es que var su nivel de scope siempre será global, estemos en el nivel que estemos siempre podremos acceder a esa variable.
+
+//¿qué es el ámbito de aplicación?
+//Respuesta ->en esta pregunta aun tengo duda...
+
+//¿cuáles son los tipos de scope?
+//Respuesta -> global y de bloque.
+
+//¿cuál es la diferencia entre "===" y "=="?
+//Respuesta ->  "===" el valor tiene que ser estrictamente igual "==" el valor puede ser igual
+
+//¿cuál es la diferencia entre "!==" y "!="?
+//Respuesta -> "!=="" si es estrictamente diferente "!=" es diferente.
+
+//¿cuál es la diferencia entre "===" y "!=="?
+//Respuesta ->  "===" tiene que estrictamente igual "!==" es diferente o igual.
+
+//¿cuál es la diferencia entre "==" y "!="?
+//Respuesta ->  "==" es igual pero no de manera estricta "!==" si es diferen o igual.
+
+//Create one variable with let called name and set your name
+
+let name = 'Andres';
+console.log(name);
+
+//Create one variable with const called nameConst and set your name
+
+const nameConst = 'Andres';
+console.log(nameConst);
+
+//Create one variable with var called nameVar and set your name
+
+var nameVar = 'Andres';
+console.log(nameVar);
+
+//Create one variable with let called age without value
+
+let age;
+console.log(age);
+
+//Create one variable with const called ageConst without value
+const ageConst = 0;
+console.log(ageConst);
+
+//Create one variable with var called ageVar without value
+
+var ageVar;
+console.log(ageVar);
+
+//Assign to each age variable your age
+
+console.log((age = 26));
+
+//console.log((ageConst = 26));
+
+console.log((ageVar = 26));
+
+//Print in console the evaluation of something with "===" that returns true
+
+console.log(1 === 1);
+
+//Print in console the evaluation of something with "===" that returns false
+
+console.log(1 === '1');
+
+//Print in console the evaluation of something with "==" that returns true
+
+console.log(1 == '1');
+
+//Print in console the evaluation of something with "==" that returns false
+
+console.log(1 == 2);
+
+//Print in console the evaluation of something with "!==" that returns true
+
+console.log(1 !== '1');
+
+//Print in console the evaluation of something with "!=" that returns false
+
+console.log(1 != 1);
+
+//When the task has been completed you have to make a PR to develop
