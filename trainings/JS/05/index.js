e6a5ed4caa3540eff1d55dd02c08b7b7d3b925bf
Andres Soto <andressoto@MacBook-Air-de-Edgar.local>
2022-09-23 10:07:28 -0500

    perf(index.js): ejerciciojs5terminado

---
+++ trainings/JS/05/index.js
@@ -0,0 +1,264 @@
+// Modify all 'you' to 'I' from 'You step into the Road, and if you don’t keep your feet, there is no knowing where you might be swept off to' and print it (All 'you' should be changed no matter if it is in Upper or Lower case)
+
+let str =
+  'You step into the Road, and if you don’t keep your feet, there is no knowing where you might be swept off to';
+
+console.log(str.toLowerCase().replace('you', 'I'));
+
+// Find the last letter of 'Harry Potter and the half-blood prince' and print it
+
+let str1 = 'Harry Potter and the half-blood prince';
+
+console.log(str1[lenght - 1]);
+
+//Create a variable that stores an empty array
+
+let arr = [];
+
+//Create a variable that stores an array with a string, a boolean and a number as values
+
+let user = ['Andres', true, 26];
+
+//Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica. Access to the array and print Alejandra
+
+let names = [
+  'Edgar',
+  'Andres,',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+console.log(names[2]);
+
+//Create an array from the next string 'Fast and Furious', the array should contain only words.
+
+let picture = ['Fast an the Furious'];
+
+//Create an array with the next structure
+
+let users = [
+  ['Edgar', 28, 'man'],
+  ['Bob', 'Elmo', 'Mia'],
+  ['Alejandra', 22, 'Woman'],
+  ['Gyda', 'Godard'],
+  ['Andres', 26, 'Man'],
+  ['Shamid'],
+  ['Harumi', 26, 'Woman'],
+  ['Dante'],
+];
+
+console.log(users[5][0]);
+console.log(users[3][0]);
+console.log(users[2][1]);
+
+// Create a variable with the next string 'Harry'
+// Create a variable with the next string 'Potter'
+// Print this string 'Harry Potter and the Goblet of fire' using the previous variables on template
+
+let name = 'Harry';
+
+let lastName = 'Potter';
+
+console.log(`${name} ${lastName} y el caliz de fuego`);
+
+//Create a variable with the next string 'Harry Potter and the prisoner of Azkaban'
+// Reverse the string to this -> "nabakzA fo renosirp eht dna rettoP yrraH"
+
+let name1 = 'Harry Poteer and the prisioner of Azkaban';
+
+console.log(name1.split());
+
+//Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica and sort it
+
+let peoples = [
+  'Edgar',
+  'Andres',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+console.log(peoples.sort());
+
+//Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica and add at the end 'Shamid'
+
+let peoples1 = [
+  'Edgar',
+  'Andres',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+let add = peoples1.push('Shamid');
+
+console.log(peoples1);
+
+//Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica and add at the beginning 'Dante'
+
+let peoples2 = [
+  'Edgar',
+  'Andres',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+let add1 = peoples2.unshift('Dante');
+
+console.log(peoples2);
+
+// Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica and remove the last item
+
+let peoples3 = [
+  'Edgar',
+  'Andres',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+let delet = peoples3.pop();
+
+console.log(peoples3);
+
+//Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica and remove the first item
+
+let peoples4 = [
+  'Edgar',
+  'Andres',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+let delet1 = peoples4.shift();
+
+console.log(peoples4);
+
+// Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica and print only 'Alejandra, Harumi'
+
+let peoples5 = [
+  'Edgar',
+  'Andres',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+console.log(peoples5[2], peoples5[3]);
+
+// Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica and replace Alejandro for 'Shamid'
+
+let peoples6 = [
+  'Edgar',
+  'Andres',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+let replace = peoples6.splice(4, 1, 'Shamid');
+
+console.log(peoples6);
+
+//Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica and replace 'Alejandro, Veronica' for 'Dante'
+
+let peoples7 = [
+  'Edgar',
+  'Andres',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+let replace1 = peoples7.splice(4, 2, 'Dante');
+
+console.log(peoples7);
+
+// //Create an array with 'Harry'
+// Create an array with 'Ron'
+// Create an array with 'Hermione'
+// Mix the last three variables
+
+let n1 = ['Harry'];
+let n2 = ['Ron'];
+let n3 = ['Hermione'];
+
+console.log(n1.concat(n2, n3));
+
+// Create an array with these values Edgar, Andres, Alejandra, Harumi and validate if 'Gyda' is in the array
+
+let arr1 = ['Edgar', 'Andres', 'Alejandra', 'Harumi'];
+
+console.log(arr1.includes('Gyda'));
+
+//Create an array with these values Edgar, Andres, Alejandra, Harumi and validate if the array is empty or not
+
+let arr2 = ['Edgar', 'Andres', 'Alejandra', 'Harumi'];
+
+if (arr2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro and print the first item, the middle item and the last item (prints should be in one line)
+
+let arr3 = ['Edgar', 'Andres', 'Alejandra', 'Harumi'];
+
+console.log(arr3[0][2][3]);
+
+// Create an array with these values Edgar, Andres, Alejandra, Harumi, Alejandro, Veronica and add 'Shamid, Dante' between 'Harumi, Alejandro'
+// Expected output -> Edgar, Andres, Alejandra, Harumi, Shamid, Dante, Alejandro, Veronica
+
+let peoples8 = [
+  'Edgar',
+  'Andres',
+  'Alejandra',
+  'Harumi',
+  'Alejandro',
+  'Veronica',
+];
+
+let add2 = peoples8.console.log(peoples8);
+
+// Create a variable called score and assign one number between 0 and 10, use 2 decimals
+// Evaluate the score against 6
+// If the score is greater or equal than six print 'Success'
+// If the score is lower than six print 'Fail'
+// You have to use some method to round score
+// Repeat the evaluation using 4 as magic number and use some method to round up the score
+// Repeat the evaluation using 2 as magic number and use some method to round down the score
+// Print a random number between 0 and 10 without using decimals
+// Print a random number between 5 and 10 without using decimals
+// Create an array with the next values: 10, 25, 55, 32, 78, 94, 24
+// Create a random number that does not exceed the length of the array
+// Print the array value selected based on the random number
+// Repeat the last exercise using strings as array values
+
+let score = 8.9;
+
+if (score >= 6) {
+  console.log('Éxito');
+} else {
+  console.log('Fallo');
+}
+
+console.log(Math.floor(score));
+console.log(Math.ceil(score));
+
+console.log(Math.random() * 10);
+console.log(Math.random(5) * 10);
+
+let numArray = [10, 25, 55, 32, 78, 94, 24];
