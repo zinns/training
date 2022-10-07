18636f318ad3546c7b3864784dc74c896373d200
Andres Soto <andressoto@MacBook-Air-de-Edgar.local>
2022-10-07 14:22:14 -0500

    fix(commits): remove double slashes for comments

--- trainings/JS/01/index.js
+++ trainings/JS/01/index.js
@@ -18,78 +18,78 @@
 // Respuesta -> CONST: es una variable la cual su valor no puede cambiar.
 // Respuesta ->  VAR: es una variable que puede cmbiar su valor, la diferencia entre let y var es que var su nivel de scope siempre será global.
 
-// //¿qué es el ámbito de aplicación?
-// //Respuesta -> el scope es el nivel en el que podrá ser accesible a la variable que deseamos.
+// ¿qué es el ámbito de aplicación?
+// Respuesta -> el scope es el nivel en el que podrá ser accesible a la variable que deseamos.
 
-// //¿cuáles son los tipos de scope?
-// //Respuesta -> global y de bloque.
+// ¿cuáles son los tipos de scope?
+// Respuesta -> global y de bloque.
 
-// //¿cuál es la diferencia entre "===" y "=="?
-// //Respuesta ->  "===" el valor tiene que ser estrictamente igual ejemplo:
+// ¿cuál es la diferencia entre "===" y "=="?
+// Respuesta ->  "===" el valor tiene que ser estrictamente igual ejemplo:
 // console.log(1 === 1);
 // console.log(true === true);
 // console.log('hola' === 'hola');
-// // "==" en esta condición el valor es igual un ejemplo 0 u "0" aun que sea un cero e estring al volverlo un valor común el final de resultado es true
+//  "==" en esta condición el valor es igual un ejemplo 0 u "0" aun que sea un cero e estring al volverlo un valor común el final de resultado es true
 // console.log(1 == '1');
 // console.log(0 == false);
 
-// //¿cuál es la diferencia entre "!==" y "!="?
-// //Respuesta -> "!=="" si es estrictamente diferente ejemplo:
-// //  si realizamos una comparacion de 1 !== '1' -> resultdo: true (ya que en realidad son diferentes y que uno es un numero y el otro es un string) ejemplo:
+// ¿cuál es la diferencia entre "!==" y "!="?
+// Respuesta -> "!=="" si es estrictamente diferente ejemplo:
+//   si realizamos una comparacion de 1 !== '1' -> resultdo: true (ya que en realidad son diferentes y que uno es un numero y el otro es un string) ejemplo:
 // console.log(1 !== '1');
 // console.log('Hola' !== 'hola');
 // console.log(true !== false);
 
-// // "!=" no es igual ejemplo:
-// //si realizamos una comparacion de 1 != '1' -> resultado: false (regresa falso, ya que al volver el estring a un valor comun es 1)
+//  "!=" no es igual ejemplo:
+// si realizamos una comparacion de 1 != '1' -> resultado: false (regresa falso, ya que al volver el estring a un valor comun es 1)
 // console.log(1 != '1');
 // console.log('hola' != 'hola');
 // console.log(0 != false);
-// //¿cuál es la diferencia entre "===" y "!=="?
-// //Respuesta ->  "===" tiene que estrictamente
-// //el valor tiene que ser estrictamiente igual al que hacemos la comparación, ya sea numero string o boleano ejemplo:
+// ¿cuál es la diferencia entre "===" y "!=="?
+// Respuesta ->  "===" tiene que estrictamente
+// el valor tiene que ser estrictamiente igual al que hacemos la comparación, ya sea numero string o boleano ejemplo:
 // console.log(1 === 1);
 // console.log('hola' === 'hola');
 // console.log(true === true);
 
-// // igual "!==" es diferente o igual.
-// // tiene que ser estrictamente diferente al valor con el que comparamos ejemplo:
+//  igual "!==" es diferente o igual.
+//  tiene que ser estrictamente diferente al valor con el que comparamos ejemplo:
 // console.log(1 !== '1');
 // console.log('Hola' !== 'hola');
 // console.log(true !== false);
 
-// //¿cuál es la diferencia entre "==" y "!="?
-// //Respuesta ->  "==" es igual pero no de manera estricta. Por lo que al comparar un valor numerico con un estring de número devolver true al convertirlo en un valor común ejemplo:
+// ¿cuál es la diferencia entre "==" y "!="?
+// Respuesta ->  "==" es igual pero no de manera estricta. Por lo que al comparar un valor numerico con un estring de número devolver true al convertirlo en un valor común ejemplo:
 // console.log(1 == '1');
 // console.log('hola' == 'hola');
 // console.log(0 == false);
 
-// // "!=" si es diferen pero no de manera estricta.
+//  "!=" si es diferen pero no de manera estricta.
 // console.log(1 != '1');
 // console.log('hola' != 'hola');
 // console.log(0 != false);
 
-// //Create one variable with let called name and set your name
+// Create one variable with let called name and set your name
 
 // let name = 'Andres';
 // console.log(name);
 
-// //Create one variable with const called nameConst and set your name
+// Create one variable with const called nameConst and set your name
 
 // const nameConst = 'Andres';
 // console.log(nameConst);
 
-// //Create one variable with var called nameVar and set your name
+// Create one variable with var called nameVar and set your name
 
 // var nameVar = 'Andres';
 // console.log(nameVar);
 
-// //Create one variable with let called age without value
+// Create one variable with let called age without value
 
 // let age;
 // console.log(age);
 
-// //Create one variable with const called ageConst without value
+// Create one variable with const called ageConst without value
 // const ageConst;
 // console.log(ageConst);
 
