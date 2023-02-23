8cdadccdaa41e6d29d76b8b34021c713ea3994e3
Edgar Zea <cesszea@icloud.com>
2023-02-23 11:11:57 -0600

    Revert "Andres.soto.andressotocruz"

--- automation/status.mjs
+++ automation/status.mjs
@@ -6,23 +6,26 @@ import { differenceInBusinessDays } from 'date-fns';
 import axios from 'axios';
 
 const getMissingStatuses = async () => {
-  console.log(process.env.GH_TOKEN);
-  console.log(process.env.TELEGRAM_TOKEN);
-  console.log(process.env.ZINNS_TELEGRAM_CHAT_ID);
+  const fullDate = new Date();
+  const year = fullDate.getFullYear();
+  const month = fullDate.getMonth() + 1;
+
   try {
     const octokit = new Octokit({
       auth: `${process.env.GH_TOKEN}`,
       userAgent: 'zinns integration',
     });
 
-    const { data } = await octokit.rest.issues.listComments({
+    const { data: status } = await octokit.rest.issues.listComments({
       issue_number: '40' /* eslint-disable-line camelcase */,
       owner: 'zinns',
       repo: 'training',
+      since: new Date(`${year}-${month}-1`),
     });
 
     const today = new Date();
-    const lastComment = new Date(data[data.length - 1].created_at);
+    const lastComment =
+      status.length > 0 ? new Date(status[status.length - 1].created_at) : fullDate;
 
     const missingStatuses = differenceInBusinessDays(today, lastComment);
 
