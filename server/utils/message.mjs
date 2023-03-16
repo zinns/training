f6a9427d5dfc560bd877d4aed19b9691433626e9
Edgar Zea <cesszea@icloud.com>
2023-03-16 13:48:51 -0600

    feat(server): improve webhook messages

--- server/utils/message.mjs
+++ server/utils/message.mjs
@@ -44,25 +44,31 @@ export const validatePayload = (payload, update) => {
 };
 
 export const createDescription = (payload, update) => {
-  return update.length === 1
-    ? {
-        check_run: ` \\-\\> ${payload?.check_run?.name} \\-\\> status: ${payload?.check_run?.status} \\-\\> conclusion: ${payload?.check_run?.conclusion}`,
-        check_suite: ` \\-\\> ${payload?.check_suite?.head_branch} \\-\\> status: ${payload?.check_suite?.status} \\-\\> conclusion: ${payload?.check_suite?.conclusion}`,
-        issue: ` \\-\\> name: ${payload?.issue?.title}`,
-        pull_request: ` \\-\\> ${payload?.pull_request?.state} \\-\\> merged: ${
-          payload?.pull_request?.merged
-        } ${
-          payload?.pull_request?.merged
-            ? ` \\-\\> merged by: ${payload?.pull_request?.merged_by.login}`
-            : ''
-        }`,
-        push: ` \\-\\> ${payload?.commits?.length} ${
-          payload?.forced ? 'forced commit(s)' : 'commit(s)'
-        } \\-\\> ${payload?.ref?.split('/').splice(2).join('')}`,
-        workflow_job: ` \\-\\> ${payload?.workflow_job?.head_branch} \\-\\> job: ${payload?.workflow_job?.name} \\-\\> status: ${payload?.workflow_job?.status} \\-\\> conclusion: ${payload?.workflow_job?.conclusion}`,
-        workflow_run: ` \\-\\> ${payload?.workflow_run?.head_branch} \\-\\> status: ${payload?.workflow_run?.status} \\-\\> conclusion: ${payload?.workflow_run?.conclusion}`,
-      }[update[0]]
-    : null;
+  return {
+    check_run: ` \\-\\> ${payload?.check_run?.name} \\-\\> status: ${payload?.check_run?.status} \\-\\> conclusion: ${payload?.check_run?.conclusion}`,
+    check_suite: ` \\-\\> ${payload?.check_suite?.head_branch} \\-\\> status: ${payload?.check_suite?.status} \\-\\> conclusion: ${payload?.check_suite?.conclusion}`,
+    issue: ` \\-\\> name: ${payload?.issue?.title} ${
+      update?.[1] ? `\\-\\> action: ${update[1]}` : ''
+    } `,
+    pull_request: ` \\-\\> ${payload?.pull_request?.state} \\-\\> merged: ${
+      payload?.pull_request?.merged
+    } ${
+      payload?.pull_request?.merged
+        ? ` \\-\\> merged by: ${payload?.pull_request?.merged_by.login} \\-\\> reviewers: ${
+            payload?.pull_request?.requested_reviewers.length > 0
+              ? payload?.pull_request?.requested_reviewers.join(', ')
+              : payload?.pull_request?.requested_reviewers.length
+          } \\-\\> labels: ${
+            payload?.pull_request?.labels.length > 0 ? payload?.pull_request?.labels.join(', ') : 0
+          }`
+        : ''
+    }`,
+    push: ` \\-\\> ${payload?.commits?.length} ${
+      payload?.forced ? 'forced commit(s)' : 'commit(s)'
+    } \\-\\> ${payload?.ref?.split('/').splice(2).join('')}`,
+    workflow_job: ` \\-\\> ${payload?.workflow_job?.head_branch} \\-\\> job: ${payload?.workflow_job?.name} \\-\\> status: ${payload?.workflow_job?.status} \\-\\> conclusion: ${payload?.workflow_job?.conclusion}`,
+    workflow_run: ` \\-\\> ${payload?.workflow_run?.head_branch} \\-\\> status: ${payload?.workflow_run?.status} \\-\\> conclusion: ${payload?.workflow_run?.conclusion}`,
+  }[update[0]];
 };
 
 export const formatMessage = (actor, description, location, update) => {
