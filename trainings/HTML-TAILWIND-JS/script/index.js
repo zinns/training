aedd1f7a972a2b9d01b0bd835af8022c4397c689
andres-soto <andysoto98811@gmail.com>
2023-04-20 16:06:27 -0600

    feat(animation tab): hover error correction

--- trainings/HTML-TAILWIND-JS/script/index.js
+++ trainings/HTML-TAILWIND-JS/script/index.js
@@ -8,8 +8,10 @@ tabs.forEach(tab => {
 
     tabs.forEach(cap => {
       cap.classList.remove('bg-red', 'text-white');
+      cap.classList.add('hover:bg-red-100', 'hover:text-blue');
     });
 
     position.classList.add('bg-red', 'text-white');
+    position.classList.remove('hover:bg-red-100', 'hover:text-blue');
   });
 });
