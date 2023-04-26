d7d3c061ee7ffddef5985007c2307617559d5556
andres-soto <andysoto98811@gmail.com>
2023-04-26 17:41:41 -0600

    feat(tab): change animations

--- trainings/HTML-TAILWIND-JS/script/index.js
+++ trainings/HTML-TAILWIND-JS/script/index.js
@@ -1,11 +1,17 @@
 const tabs = document.querySelectorAll('#capsule');
 
-console.log(tabs);
+const btnActive = () => {
+  const btn = true;
 
-tabs.forEach(ele => {
-  ele.addEventListener('click', () => {
-    ele.addEventListener('mouseenter', () => {
-      ele.classList.toggle('hover:bg-transparent');
+  tabs.forEach(ele => {
+    ele.addEventListener('click', () => {
+      if (ele === 'click') {
+        ele.classList.add('hover:bg-transparent');
+      } else {
+        ele.classList.add('hover:bg-red-100', 'hover:text-blue');
+      }
     });
   });
-});
+};
+
+btnActive();
