5e45de797e8405cdade594c1e84c53abf8958f3d
andres-soto <andysoto98811@gmail.com>
2023-04-28 12:26:32 -0600

    feat(list-pokemons): added to pokemon list

--- trainings/HTML-TAILWIND-JS/script/index.js
+++ trainings/HTML-TAILWIND-JS/script/index.js
@@ -1,15 +1,40 @@
+const pokemonContainer = document.querySelector('#pokemon-container');
+
 const pokedex = id => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then(res => res.json())
     .then(data => {
-      console.log(data);
+      listPokemon(data);
     });
 };
 
 const allPokemons = num => {
-  for (let i = 0; i <= num; i++) {
+  for (let i = 1; i <= num; i++) {
     pokedex(i);
   }
+};
+
+const listPokemon = pokemon => {
+  const container = document.createElement('div');
+
+  container.style.display = 'flex';
+  container.style.gap = '10px';
+
+  const number = document.createElement('p');
+
+  number.textContent = `#${pokemon.id.toString()}`;
+
+  const name = document.createElement('p');
+
+  name.classList.add('list:none');
+
+  name.classList.add('name');
+  name.textContent = pokemon.name;
+
+  container.appendChild(number);
+  container.appendChild(name);
+
+  pokemonContainer.appendChild(container);
 };
 
 allPokemons(250);
