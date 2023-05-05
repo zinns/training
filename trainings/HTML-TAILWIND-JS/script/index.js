5c238eb50ab0fac9abe94032d37c02a9514e12b7
andysotocruz <75503041+andysotocruz@users.noreply.github.com>
2023-05-04 21:54:25 -0600

    feat(added animation): added animation, merge of components to index (#146)
    * feat(added animation): added animation, merge of components to index
    
    * feat(animation tab): hover error correction
    
    * feat(animation tab): change of animation tab
    
    * feat(animation): animation is corrected and js code is deleted
    
    * feat(changes tab): changes tab
    
    * feat(tab): change animations
    
    * feat(tab): change animation
    
    * feat(change tab): change tab animation
    
    * feat(tab-animation): fixed tab animation

--- trainings/HTML-TAILWIND-JS/script/index.js
+++ trainings/HTML-TAILWIND-JS/script/index.js
@@ -1,4 +1,29 @@
+const buttons = [...document.querySelectorAll('#capsule')];
 const pokemonContainer = document.querySelector('#pokemon-container');
+
+const activeHover = ({ target }) => {
+  if (target.dataset.active === 'false' && target.dataset.hovered === 'false') {
+    target.classList.add('bg-red-100', 'text-blue');
+  }
+};
+
+const disableHover = ({ target }) => {
+  target.classList.remove('bg-red-100', 'text-blue');
+};
+
+const setActive = ({ target }) => {
+  const preActive = buttons.filter(tab => tab.dataset.active === 'true');
+
+  preActive[0].dataset.active = false;
+  target.dataset.active = true;
+  disableHover({ target });
+};
+
+buttons.forEach(tab => {
+  tab.addEventListener('mouseover', activeHover);
+  tab.addEventListener('mouseleave', disableHover);
+  tab.addEventListener('click', setActive);
+});
 
 const pokedex = id => {
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
